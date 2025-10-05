d8b1217bea82f0c87e8ef81de7ccc5dc5bc240b4
qe201020335 <qe201020335@sina.com>
2025-10-05 00:45:21 -0400

    Load beatmap data and calculate max score

--- ScorePercentage/Data/SongPlayDataProvider.cs
+++ ScorePercentage/Data/SongPlayDataProvider.cs
@@ -1,14 +1,89 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using IPA.Logging;
+using IPA.Utilities;
+using Zenject;
 
 namespace ScorePercentage.Data;
 
 internal class SongPlayDataProvider : IDataProvider
 {
+    [Inject]
+    private readonly Logger _logger = null!;
+
+    [Inject]
+    private readonly BeatmapLevelsModel _beatmapLevelsModel = null!;
+
+    [Inject]
+    private readonly BeatmapLevelsEntitlementModel _beatmapLevelsEntitlementModel = null!;
+
+    [Inject]
+    private readonly BeatmapDataLoader _beatmapDataLoader = null!;
+
+    private readonly Dictionary<BeatmapKey, int> _cache = new();
+
     public int GetHistoryHighScore(BeatmapKey beatmapKey) => 0;
 
-    //TODO
-    public Task<int> GetMaxMultipliedScore(BeatmapKey beatmapKey, CancellationToken token) => Task.FromResult(0);
+    public async Task<int> GetMaxMultipliedScore(BeatmapKey beatmapKey, CancellationToken token)
+    {
+        if (!UnityGame.OnMainThread) throw new InvalidOperationException("Must be called on main thread");
+        if (_cache.TryGetValue(beatmapKey, out var cached)) return cached;
+
+#if DEBUG
+        var startTime = DateTime.Now;
+#endif
+
+        var beatmapLevel = _beatmapLevelsModel.GetBeatmapLevel(beatmapKey.levelId);
+        if (beatmapLevel == null)
+        {
+            _logger.Error("Failed to get BeatmapLevel.");
+            throw new Exception("Failed to get BeatmapLevel.");
+        }
+
+        _logger.Trace("Loading beat map level data from BeatmapLevelsModel.");
+        var dataVersion = await _beatmapLevelsEntitlementModel.GetLevelDataVersionAsync(beatmapKey.levelId, token);
+        var loadResult = await _beatmapLevelsModel.LoadBeatmapLevelDataAsync(beatmapKey.levelId, dataVersion, token);
+        if (loadResult.isError)
+        {
+            _logger.Error("Failed to load BeatmapLevelData.");
+            throw new Exception("Failed to load BeatmapLevelData.");
+        }
+
+        var beatmapLevelData = loadResult.beatmapLevelData!;
+
+        var beatmapData = await _beatmapDataLoader.LoadBeatmapDataAsync(
+            beatmapLevelData,
+            beatmapKey,
+            beatmapLevel.beatsPerMinute,
+            false,
+            null,
+            null,
+            dataVersion,
+            null,
+            null,
+            false);
+
+        token.ThrowIfCancellationRequested();
+
+        if (beatmapData == null)
+        {
+            _logger.Error("Failed to get BeatmapData.");
+            throw new Exception("Failed to get BeatmapData.");
+        }
+
+        var maxScore = ScoreModel.ComputeMaxMultipliedScoreForBeatmap(beatmapData);
+
+#if DEBUG
+        var timeSpan = DateTime.Now - startTime;
+        _logger.Info(
+            $"Took {timeSpan.TotalSeconds:F3} sec to load scoring data for {beatmapLevel.songName} ({beatmapKey.beatmapCharacteristic.compoundIdPartName}{beatmapKey.difficulty.SerializedName()})");
+#endif
+
+        _cache[beatmapKey] = maxScore;
+        return maxScore;
+    }
 
     //TODO
     public LevelResultsExtraData? GetLevelCompletionResultsExtraData(LevelCompletionResults results) => null;
---
+++ ScorePercentage/Patches/BeatmapLoadingPatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace ScorePercentage.Patches;
+
+//Modified from MappingExtensions
+[HarmonyPatch(typeof(BeatmapObjectsInTimeRowProcessor),
+    nameof(BeatmapObjectsInTimeRowProcessor.HandleCurrentTimeSliceAllNotesAndSlidersDidFinishTimeSlice))]
+internal static class BeatmapLoadingPatch
+{
+    private static readonly MethodInfo ClampMethod = AccessTools.Method(typeof(Math), nameof(Math.Clamp),
+        new[] { typeof(int), typeof(int), typeof(int) });
+
+    [HarmonyPrepare]
+    // MappingExtensions' patch will throw if we happen to patch it first
+    private static bool Prepare() => !Plugin.Instance.MappingExtensionsInstalled;
+
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var instructs = new List<CodeInstruction>(instructions);
+        var patched = instructs.Exists(instruction => instruction.Calls(ClampMethod));
+        if (patched)
+        {
+            Plugin.Logger.Debug("BeatmapObjectsInTimeRowProcessor is already patched. Skipping ScorePercentage patch");
+            return instructs;
+        }
+
+        Plugin.Logger.Debug("Patching BeatmapObjectsInTimeRowProcessor");
+        // Prevents an IndexOutOfRangeException when processing precise line indexes.
+        var matcher = new CodeMatcher(instructs);
+        matcher.MatchEndForward(
+                new CodeMatch(OpCodes.Ldloc_S),
+                new CodeMatch(OpCodes.Callvirt),
+                new CodeMatch(OpCodes.Ldelem_Ref))
+            .ThrowIfInvalid("Failed to patch BeatmapObjectsInTimeRowProcessor.")
+            .Insert(
+                new CodeInstruction(OpCodes.Ldc_I4_0),
+                new CodeInstruction(OpCodes.Ldc_I4_3),
+                new CodeInstruction(OpCodes.Call, ClampMethod));
+
+        Plugin.Logger.Debug("BeatmapObjectsInTimeRowProcessor patched");
+        return matcher.InstructionEnumeration();
+    }
+}
